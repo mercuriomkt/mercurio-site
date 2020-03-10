2711e6a200fde565d220e79bf19872437e371c59
Willian Amaral <willian.amaral@outlook.com.br>
2020-03-10 16:09:06 -0300

    Updates

---
+++ precache-manifest.365a7ddc84cb53d17f1103195e52d2fe.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40bbdd2311ba6bb89b2feda8726b87ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "29d8e5962dff94b63fb3",
+    "url": "/static/css/main.35a09a74.chunk.css"
+  },
+  {
+    "revision": "7aa732539324ef70aad1",
+    "url": "/static/js/2.13e132b1.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.13e132b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29d8e5962dff94b63fb3",
+    "url": "/static/js/main.b6c70e14.chunk.js"
+  },
+  {
+    "revision": "c19ffe3c4a91e35e004d",
+    "url": "/static/js/runtime-main.f7f06b0e.js"
+  },
+  {
+    "revision": "526ab6ad8c712684f5fc17cc460a23d4",
+    "url": "/static/media/bg.526ab6ad.jpg"
+  },
+  {
+    "revision": "c70a5bee7e2f64f5b734313e7937af85",
+    "url": "/static/media/close.c70a5bee.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b5de90bd6babc14485fda20bb9e3c472",
+    "url": "/static/media/pic01.b5de90bd.jpg"
+  },
+  {
+    "revision": "7b71e6debe5beb3bc49af43e1d0c15ff",
+    "url": "/static/media/pic02.7b71e6de.jpg"
+  },
+  {
+    "revision": "a6b15cf5ee7dc7e0cd9630a1242cc500",
+    "url": "/static/media/pic03.a6b15cf5.jpg"
+  },
+  {
+    "revision": "8d621d28381af5bce0d831504fbec218",
+    "url": "/static/media/pic04.8d621d28.jpg"
+  }
+]);
